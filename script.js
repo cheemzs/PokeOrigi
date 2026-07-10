761e9843241157ba2dedec3508854a436fcd87a1
cheemzs <lucas.cheam@gmail.com>
2026-07-10 22:34:19 +0700

    Add button click event to display Pokémon search results
    Fix issues with element retrieval and event listener.

---
+++ script.js
@@ -0,0 +1,8 @@
+const myButton = document.getElementById(goBtn);
+const myResult = document,getElementById(displayResults);
+
+myButton.addEventListener("click", function() {
+  const pokemonName = document.getElementById(cardName).value;
+  const setName = document.getElementByIf(setName).value;
+  myResult.innerHTML = "You searched for" + pokemonName + "from set" + setName + "."
+})
